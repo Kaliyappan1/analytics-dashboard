d664a800b5ba9183f02e99af8e0661e43ef82e39
kaliyappan <yappank17@gmail.com>
2024-10-10 17:33:27 +0530

    analytics ui is completed

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -1,13 +1,15 @@
 // src/components/Metrics.jsx
 import React from "react";
-import "../assets/styles/Metrics.css"
+import "../assets/styles/Metrics.css";
 import { FaDollarSign, FaClock, FaRegTimesCircle } from "react-icons/fa";
 
 const Metrics = () => {
   return (
     <div className="metrics">
       <div className="metric">
-        <FaDollarSign size={30} className="icon"/>
+        <div className="icon-container-income">
+          <FaDollarSign size={30} color="#01B651" />
+        </div>
         <div>
           <h4>Total Requests</h4>
           <p>120k</p>
@@ -15,7 +17,9 @@ const Metrics = () => {
       </div>
       <span className="metric-break-line"></span>
       <div className="metric">
-        <FaClock size={30} className="icon"/>
+        <div className="icon-container-clock">
+          <FaClock size={30} color="#1268DC" />
+        </div>
         <div>
           <h4>Avg. Response Time</h4>
           <p>1.2s</p>
@@ -23,7 +27,9 @@ const Metrics = () => {
       </div>
       <span className="metric-break-line"></span>
       <div className="metric">
-        <FaRegTimesCircle size={30} className="icon"/>
+        <div className="icon-container-time">
+          <FaRegTimesCircle size={30} color="#F6000E" />
+        </div>
         <div>
           <h4>Failed Requests</h4>
           <p>178</p>
--- src/components/Requests.js
+++ src/components/Requests.js
@@ -10,16 +10,13 @@ const Requests = () => {
         </div>
         <div className="search-bar">
           <CgSearch size={22} className="search-icon" />
-          <input type="text" placeholder="Search" style={{ margin:"0 5px"}}/>  
-          <input type="date" placeholder="Last 30 days" />  
-
-
+          <input type="text" placeholder="Search" style={{ margin: "0 5px" }} />
+          <input type="date" placeholder="Last 30 days" />
         </div>
-
       </div>
       <table>
         <thead>
-          <tr >
+          <tr>
             <th>ID</th>
             <th>Req. ID</th>
             <th>Req. Status</th>
@@ -52,6 +49,50 @@ const Requests = () => {
             <td>2024-05-28 22:12:01Z</td>
             <td>Application/JSON</td>
             <td>192.167.1.1</td>
+            <td>Windows</td>
+            <td>Chrome</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Req-3</td>
+            <td>500</td>
+            <td>POST</td>
+            <td>3034-05-38 33:13:01Z</td>
+            <td>Application/JSON</td>
+            <td>193.167.1.1</td>
+            <td>Windows</td>
+            <td>Chrome</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Req-4</td>
+            <td>500</td>
+            <td>POST</td>
+            <td>4044-05-48 44:14:01Z</td>
+            <td>Application/JSON</td>
+            <td>194.167.1.1</td>
+            <td>Windows</td>
+            <td>Chrome</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Req-5</td>
+            <td>500</td>
+            <td>POST</td>
+            <td>5055-05-58 55:15:01Z</td>
+            <td>Application/JSON</td>
+            <td>195.167.1.1</td>
+            <td>Windows</td>
+            <td>Chrome</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Req-6</td>
+            <td>500</td>
+            <td>POST</td>
+            <td>6066-05-68 66:16:01Z</td>
+            <td>Application/JSON</td>
+            <td>196.167.1.1</td>
             <td>Windows</td>
             <td>Chrome</td>
           </tr>
